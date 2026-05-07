de39ce633f863c2e537b49c908e3eff98a2d1213
Keshav03-P <keshavpatil1438@gmail.com>
2026-05-07 22:13:25 +0530

    Update skills.py

--- skills.py
+++ skills.py
@@ -3,7 +3,23 @@
 SKILLS = [
     "python","java","c++","sql","mysql","oracle","html","css","javascript",
     "react","node","flask","django","machine learning","data analysis",
-    "linux","git","api","aws"
+    "linux","git","api","aws"SKILLS = [
+    "python",
+    "flask",
+    "django",
+    "mysql",
+    "postgresql",
+    "rest api",
+    "bootstrap",
+    "chart.js",
+    "nlp",
+    "git",
+    "testing",
+    "authentication",
+    "authorization",
+    "tf-idf",
+    "cosine similarity"
+]
 ]
 
 #-------- SKILLS ----------
